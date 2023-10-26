dfb443b15b30df6568ba206bc41fb932bc60c626
Bohumil Brož <127795959+brozb@users.noreply.github.com>
2023-10-26 13:48:02 +0200

    use the correct rviz config file

--- controls.sh
+++ controls.sh
@@ -5,7 +5,7 @@ tmux new-session -d -s "$newsess" -n rviz
 sleep 1
 tmux send-keys -t "$newsess" "export ROS_MASTER_URI=http://spot-arm-1:11311/" Enter
 tmux send-keys -t "$newsess" "source ~/catkin_ws/devel/setup.bash" Enter
-tmux send-keys -t "$newsess" "roslaunch spot_viz view_robot.launch" Enter
+tmux send-keys -t "$newsess" "rviz -d ~/catkin_ws/src/spot_collector/controls.rviz" Enter
 tmux new-window -t "$newsess" -n interf
 sleep 1
 tmux send-keys -t "$newsess" "export ROS_MASTER_URI=http://spot-arm-1:11311/" Enter
