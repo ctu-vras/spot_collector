31f3fa575e5940e2c9bddfe84f25b8c2728e9251
Bohumil Brož <127795959+brozb@users.noreply.github.com>
2023-11-23 15:40:45 +0100

    set workspace as a variable

--- controls.sh
+++ controls.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 newsess="controls"
+ws="~/catkin_ws"
 
 tmux new-session -d -s "$newsess" -n rviz
 sleep 1
 tmux send-keys -t "$newsess" "export ROS_MASTER_URI=http://spot-arm-1:11311/" Enter
-tmux send-keys -t "$newsess" "source ~/catkin_ws/devel/setup.bash" Enter
-tmux send-keys -t "$newsess" "rviz -d ~/catkin_ws/src/spot_collector/controls.rviz" Enter
+tmux send-keys -t "$newsess" "source $ws/devel/setup.bash" Enter
+tmux send-keys -t "$newsess" "rviz -d $ws/src/spot_collector/controls.rviz" Enter
 tmux new-window -t "$newsess" -n interf
 sleep 1
 tmux send-keys -t "$newsess" "export ROS_MASTER_URI=http://spot-arm-1:11311/" Enter
-tmux send-keys -t "$newsess" "source ~/catkin_ws/devel/setup.bash" Enter
+tmux send-keys -t "$newsess" "source $ws/devel/setup.bash" Enter
 tmux send-keys -t "$newsess" "rosrun spot_collector interface.py" Enter
 tmux attach -t "$newsess"
